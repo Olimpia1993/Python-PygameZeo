d4c1323ec3f0d5554ca3bd7cfe1193bdcae712d0
Olimpia Michaelides <13sta20@gmail.com>
2022-12-24 00:49:12 +0100

    update

---
+++ os.system('csl') - czyszczenie konsoli.py
@@ -0,0 +1,23 @@
+import os
+from time import sleep
+
+# some text
+print("a")
+print("b")
+print("c")
+print("d")
+print("e")
+print("Screen will now be cleared in 5 Seconds")
+
+# Waiting for 5 seconds to clear the screen
+sleep(5)
+
+# Clearing the Screen
+os.system('cls')
+
+print("a")
+print("b")
+print("c")
+print("d")
+print("e")
+sleep(10)
